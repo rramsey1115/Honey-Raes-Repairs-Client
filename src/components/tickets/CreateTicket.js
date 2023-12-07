ff23550e3ff084e65d1f30ec0dd00ae222732f5d
Ryan Ramsey <rramsey1115@gmail.com>
2023-12-07 14:47:41 -0600

    initial commit of form branch

--- src/components/tickets/CreateTicket.js
+++ src/components/tickets/CreateTicket.js
@@ -1,9 +1,13 @@
 export default function CreateTicket() {
-  return (<form>
-    <h3>Submit a Ticket</h3>
+  return (<form><br/>
+    <h3>Submit a Ticket</h3><br/>
     <fieldset>
-      <label/>Description of Issue<br/>
+      <label/><h4>Description of Issue</h4>
       <input type="text"/>
-    </fieldset>
+    </fieldset><br/>
+    <fieldset>
+      <label/>Customer Submitting Issue<br/>
+      <input name="customer" type="radio" />Customer 1
+    </fieldset><br/>
   </form>)
 }
