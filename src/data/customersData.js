a6cfc1f64fd3cf49ef42a68a670f407c08b89510
Ryan Ramsey <rramsey1115@gmail.com>
2023-12-07 14:31:28 -0600

    customers views complete

--- src/data/customersData.js
+++ src/data/customersData.js
@@ -3,3 +3,7 @@ const _apiUrl = "/customers";
 export const getAllCustomers = () => {
     return fetch(_apiUrl).then(res => res.json());
 }
+
+export const getCustomerById = (customerId) => {
+    return fetch(`${_apiUrl}/${customerId}`).then(res => res.json());
+}
