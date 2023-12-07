00cd3aec6aa2e113d36ea4571094729ea3b43bc3
Ryan Ramsey <rramsey1115@gmail.com>
2023-12-07 15:51:18 -0600

    form there, not functional

--- src/components/tickets/CreateTicket.js
+++ src/components/tickets/CreateTicket.js
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
+import { getAllCustomers } from "../../data/customersData";
+
 export default function CreateTicket() {
+  const [customers, setCustomers] = useState([]);
+  const [ticketObj, setTicketObj] = useState({
+    description: "",
+    customerId: 0,
+    employeeId: null,
+    emergenccy: false,
+  });
+
+  useEffect(()=> {
+    getAllCustomers().then((data) => setCustomers(data))
+  }, [])
+
   return (<form><br/>
-    <h3>Submit a Ticket</h3><br/>
+    <h3>Submit a Ticket</h3><hr/>
+    
     <fieldset>
-      <label/><h4>Description of Issue</h4>
-      <input type="text"/>
-    </fieldset><br/>
+      <label/><h5>Customer Submitting Ticket</h5>
+        {customers.map(c => {
+          return (<div key={c.id}>
+              <input name="customer" type="radio"/> {c.name}
+            </div>)
+        })}
+    </fieldset>
     <fieldset>
-      <label/>Customer Submitting Issue<br/>
-      <input name="customer" type="radio" />Customer 1
-    </fieldset><br/>
+      <label/><h5>Description of Issue</h5>
+      <textarea type="text"/>
+    </fieldset>
+    <fieldset>
+      <label /><h5>Emergency?</h5>
+      <div><input type="radio"/>True</div>
+      <div><input type="radio"/> False</div>
+    </fieldset>
   </form>)
 }
